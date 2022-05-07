00c1ec9dbc945f87be0b6fb1c0b4c5cb9c8f2775
illia <ilr00743@gmail.com>
2022-05-07 21:15:33 +0300

    Create Tap to start script

--- Assets/Scripts/UI/Shop/ShopPresenter.cs
+++ Assets/Scripts/UI/Shop/ShopPresenter.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ShopPresenter : MonoBehaviour
+public class GShopPresenter : MonoBehaviour
 {
     [SerializeField] private ShopView _shopView;
     [SerializeField] private GameObject _tapToStart;
