eb37b80197718c2ea9bde8032a4733f61cde4179
illia <ilr00743@gmail.com>
2022-05-09 19:46:25 +0300

    Some changes

--- Assets/Scripts/Camera/CameraState.cs
+++ Assets/Scripts/Camera/CameraState.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class CameraState : MonoBehaviour
 {
     [SerializeField] private TapToStartPresenter _tapToStart;
+    [SerializeField] private FinishLine _finishLine;
     private Animator _animator;
 
     private void Awake()
@@ -16,23 +17,29 @@ public class CameraState : MonoBehaviour
     private void OnEnable()
     {
         _tapToStart.Started += OnStarted;
+        _finishLine.Finished += OnFinished;
     }
 
     private void OnDisable()
     {
         _tapToStart.Started -= OnStarted;
+        _finishLine.Finished -= OnFinished;
     }
 
     private void OnStarted()
     {
         _animator.SetTrigger(AnimationParams.Run);
     }
-    
+
+    private void OnFinished()
+    {
+        _animator.SetTrigger(AnimationParams.Finish);
+    }
+
     private static class AnimationParams
     {
         public static readonly string Idle = nameof(Idle);
         public static readonly string Run = nameof(Run);
         public static readonly string Finish = nameof(Finish);
-        public static readonly string Death = nameof(Death);
     }
 }
