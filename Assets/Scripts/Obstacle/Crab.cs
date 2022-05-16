0cbbb66faa035096bb3429162a378c1c7e47a04d
illia <ilr00743@gmail.com>
2022-05-16 17:57:20 +0300

    Refactoring

--- Assets/Scripts/Obstacle/Crab.cs
+++ Assets/Scripts/Obstacle/Crab.cs
@@ -1,17 +1,31 @@
+using System;
 using UnityEngine;
 using DG.Tweening;
 
 public class Crab : Obstacle
 {
-    private void OnEnable()
+    private Tweener _tweener;
+    private Transform _transform;
+
+    private void Awake()
     {
-        transform.DOMove(new Vector3(GetTargetPositionX(1f), 0f, transform.position.z), 2.5f)
+        _transform = GetComponent<Transform>();
+    }
+
+    private void Start()
+    {
+        _tweener = _transform.DOMove(new Vector3(GetTargetPositionX(1f), 0f, transform.position.z), 2.5f)
             .SetEase(Ease.InOutSine)
-            .SetLoops(-1, LoopType.Yoyo).SetUpdate(UpdateType.Fixed);
+            .SetLoops(-1, LoopType.Yoyo).SetUpdate(UpdateType.Fixed);    
+    }
+
+    private void OnDisable()
+    {
+        _tweener.Kill();
     }
 
     private float GetTargetPositionX(float startPositionX)
     {
-        return transform.position.x < startPositionX ? 3f:-3f;
+        return _transform.position.x < startPositionX ? 3f:-3f;
     }
 }
